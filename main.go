2c5c7199f352834f7987a41304f36e84c9f423a6
Marten Meerits <marten.meerits@gmail.com>
2026-02-28 17:15:07 +0200

    finished lisaHarjumus

--- main.go
+++ main.go
@@ -12,12 +12,17 @@ import (
 func helloFunc() {
 	var valik string
 	praeguneAeg := time.Now().Format("15:04:05")
-	fmt.Println("Teretulemast, minu harjumuste jälgiasse! (palun vajuta nr 5 kui see on su esimene kord!)")
+	fmt.Println("Teretulemast, minu harjumuste jälgiasse!")
 	fmt.Println("Kell on", praeguneAeg)
+	fmt.Println("******************************")
 	fmt.Println("1. Lisa uus harjumus")
+	fmt.Println("******************************")
 	fmt.Println("2. Näita kõiki harjumusi")
+	fmt.Println("******************************")
 	fmt.Println("3. Märgi harjumus tehtuks")
+	fmt.Println("******************************")
 	fmt.Println("4. Välju")
+	fmt.Println("******************************")
 	fmt.Print("Vali tegevus: ")
 	fmt.Scanln(&valik)
 	switch valik {
@@ -30,49 +35,50 @@ func helloFunc() {
 	case "4":
 		fmt.Println("Head aega!")
 		return
-	case "5":
-		esimeneKord()
 	default:
 		fmt.Println("Vigane valik, proovi uuesti.")
 	}
 
 }
 
-func esimeneKord() {
-	db, err := sql.Open("sqlite3", "userdata.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer db.Close()
-	sql := `CREATE TABLE userdata (
-			id INTEGER PRIMATY KEY,
-			harjumuseNimi TEXT NOT NULL,
-			harjumuseAeg TEXT NOT NULL,
-			harjumusTehtud BOOL NOT NULL,
-		);`
-
-	db.Exec(sql)
-	main()
-}
-
 func lisaHarjumus() {
 	var harjumuseNimi string
 	var harjumuseAeg string
-	fmt.Scanln("Sisestage harjumuse nimi: ", &harjumuseNimi)
-	fmt.Scanln("Sisestage harjumuse aeg: ", &harjumuseAeg)
+	fmt.Print("Sisestage harjumuse nimi: ")
+	fmt.Scanln(&harjumuseNimi)
+	fmt.Print("Sisestage harjumuse aeg: ")
+	fmt.Scanln(&harjumuseAeg)
 
 	db, err := sql.Open("sqlite3", "userdata.db")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("INSERT INTO userdata (harjumuseNimi, harjumuseAeg, harjumusTehtud) VALUES (?, ?, ?)", harjumuseNimi, harjumuseAeg, false)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	defer db.Close()
+
+	tabel := `CREATE TABLE IF NOT EXISTS userdata (
+			id INTEGER PRIMATY KEY,
+			harjumuseNimi TEXT NOT NULL,
+			harjumuseAeg TEXT NOT NULL,
+			harjumusTehtud BOOL NOT NULL
+		);`
+	_, err = db.Exec(tabel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	statement, err := db.Prepare("INSERT INTO userdata (harjumuseNimi, harjumuseAeg, harjumusTehtud) VALUES (?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer statement.Close()
+
+	_, err = statement.Exec(harjumuseNimi, harjumuseAeg, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("******************************") //siin on 30 "*"
 	main()
 }
 
